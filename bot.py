489532d75b0d0d07fca6ce1d8b338fa5b776abf6
stvnrlly <stvnrlly@stvnrlly.com>
2016-06-17 15:18:31 -0400

    try until success

--- bot.py
+++ bot.py
@@ -15,18 +15,20 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# Grab one random cartoon and keep the image URL, then another and keep the caption
-image_src = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["src"]
-caption = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["caption"]
+while True:
+    # Grab one random cartoon and keep the image URL, then another and keep the caption
+    image_src = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["src"]
+    caption = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["caption"]
 
-# Unescape the HTML to convert curly quotes and such
-caption = html.unescape(caption)
+    # Unescape the HTML to convert curly quotes and such
+    caption = html.unescape(caption)
 
-# Only tweet if a caption actually exists
-if 140 > len(caption) > 1 : # use 1 instead of 0 since there's the occasional single quote caption
-    filename = "temp.jpg"
-    image = requests.get(image_src, stream=True)
-    with open(filename, "wb") as i:
-        for chunk in image:
-            i.write(chunk)
-    api.update_with_media(filename, caption)
+    # Only tweet if a caption actually exists
+    if 140 > len(caption) > 1 : # use 1 instead of 0 since there's the occasional single quote caption
+        filename = "temp.jpg"
+        image = requests.get(image_src, stream=True)
+        with open(filename, "wb") as i:
+            for chunk in image:
+                i.write(chunk)
+        api.update_with_media(filename, caption)
+        break
