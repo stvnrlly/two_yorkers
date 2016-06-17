94b7ce635feef7e3e8ae066bd689a848054bc796
stvnrlly <stvnrlly@stvnrlly.com>
2016-06-17 15:17:03 -0400

    use https

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import requests, html, json, tweepy
 
+# Get credits from a local file
 with open("creds.json") as f:
     creds = json.load(f)
 
+# Start up tweepy
 consumer_key = creds["consumer_key"]
 consumer_secret = creds["consumer_secret"]
 access_token = creds["access_token"]
@@ -13,12 +15,18 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-image_src = requests.get("http://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["src"]
-caption = requests.get("http://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["caption"]
-if 140 > len(caption) > 0 :
+# Grab one random cartoon and keep the image URL, then another and keep the caption
+image_src = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["src"]
+caption = requests.get("https://www.newyorker.com/cartoons/random/randomAPI1").json()[0]["caption"]
+
+# Unescape the HTML to convert curly quotes and such
+caption = html.unescape(caption)
+
+# Only tweet if a caption actually exists
+if 140 > len(caption) > 1 : # use 1 instead of 0 since there's the occasional single quote caption
     filename = "temp.jpg"
     image = requests.get(image_src, stream=True)
     with open(filename, "wb") as i:
         for chunk in image:
             i.write(chunk)
-    api.update_with_media(filename, html.unescape(caption))
+    api.update_with_media(filename, caption)
